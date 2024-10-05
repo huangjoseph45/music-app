3ab5ed55b71374612436906187346b4901de6b0a
huangjoseph45 <huangjoseph45@gmail.com>
2024-10-05 12:03:21 -0400

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1>Hi</h1>
+      <button>Button</button>
+      <ul>
+        <li>What is up</li>
+      </ul>
+    </>
+
+  )
+  }
+
+
+export default App
